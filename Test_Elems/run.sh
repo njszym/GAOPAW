148bd9a5a859e7f374d3def1f78188326f661111
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-05-22 18:09:58 -0400

    Further optimized dakota.in parameters and added more test elements

--- Test_Elems/run.sh
+++ Test_Elems/run.sh
@@ -1,5 +1,5 @@
 
-for elem in H Li Be B C O F Na Mg Al Si S Cl K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Rb Sr Y Zr Nb Mo Tc Ru Rh Pd
+for elem in H Li Be B C O F Na Mg Al Si S Cl K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I
 do                                                                                                                                     
 mkdir $elem                                                                                                                            
 cp dakota_input/$elem.dakota.in $elem/dakota.in                                                                                                                     

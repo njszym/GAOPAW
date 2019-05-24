07912d5ae899c901b90beda04a4b6b5b45e3b263
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-05-24 17:07:35 -0400

    Added more test elements

--- Test_Elems/run.sh
+++ Test_Elems/run.sh
@@ -1,5 +1,5 @@
 
-for elem in H Li Be B C O F Na Mg Al Si S Cl K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Cs Ba Hf Ta W Re
+for elem in H Li Be B C O F Na Mg Al Si S Cl K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Cs Ba La Hf Ta W Re Os Ir Pt Au
 do                                                                                                                                     
 mkdir $elem                                                                                                                            
 cp dakota_input/$elem.dakota.in $elem/dakota.in

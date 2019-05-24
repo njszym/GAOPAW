b0fe71fb35e8681b660c037ef4ccfb6c1620070a
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-05-24 09:38:16 -0400

    Added check_solns.py to determine accuracy of final solutions

---
+++ scripts/check_solns.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import sys
+import numpy as np
+
+
+## Determine accuracy of each final solution from a completed Dakota run
+## Usage: python check_solns.py (number of variables to check)
+
+num_obj = int(sys.argv[-1])
+df = pd.read_table('finaldata1.dat',sep='\s+',header=None)
+print ('\n',df,'\n')
+i = 0
+obj_list = []
+index = len(df.keys()) - 1
+for i in range(num_obj):
+    obj_list.append(df[index])
+    index -= 1
+obj_list = np.array(obj_list)
+obj_list = np.transpose(obj_list)
+solutions = []
+index = 1
+indices = []
+for obj in obj_list:
+    net_diff = 0
+    for value in obj:
+        net_diff += value**2
+    net_diff = net_diff**0.5
+    print (index-1, ':', net_diff)
+    solutions.append(net_diff)
+    indices.append(index)
+    index += 1
+min_diff = min(solutions)
+for (a,b) in zip(indices,solutions):
+    if b == min_diff:
+        print ('\nBest Solution: ', a, b,'\n')
+    else:
+        pass

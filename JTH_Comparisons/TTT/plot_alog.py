91af5703c4e40ed163471bd042a9a0159938aa12
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-06-06 11:49:35 -0400

    Added JTH comparisons

---
+++ JTH_Comparisons/TTT/plot_alog.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+## Gather log deriv files in directory
+## We only care about derivs up to l_max - 1
+files = os.listdir('./')
+log_derivs = []
+for file in files:
+    if file[:4] == 'logd':
+        log_derivs.append(file)
+
+
+## Begin figure
+plt.figure()
+
+## Possible orbitals and colors
+orbital_list = ['s','p','d']
+color_list = ['r','b','g']
+color_index = 0
+
+## For each l, plot exact and pseudo derivs
+for file in log_derivs[:-1]:
+    df = pd.read_table(file,sep='\s+',header=None)
+    e = df[0]
+    log_pseudo = df[4]
+    log_exact = df[3]
+    plt.plot(e,log_exact,color_list[color_index],label='Exact '+orbital_list[color_index])
+    plt.plot(e,log_pseudo,color_list[color_index],linestyle='dashed',label='Pseudo '+orbital_list[color_index])
+    color_index += 1
+
+## Some plotting features
+plt.legend(prop={'size':12},loc='lower left')
+plt.xlabel('Energy (Ry)',fontsize=16)
+plt.ylabel('Arctan(Log Deriv)',fontsize=16)
+plt.minorticks_on()
+
+plt.xlim(-1,4)
+
+## Show you the plot
+plt.savefig('Arctan.png')

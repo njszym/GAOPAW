8bc121b913020d86430f0b4e6deefa132957aed1
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-06-11 15:37:25 -0400

    Improved optimize_loggrid workflow

--- scripts/optimize_loggrid.py
+++ scripts/optimize_loggrid.py
@@ -16,10 +16,84 @@ import numpy as np
 from shutil import copyfile
 
 
-## Optimize the number of points in the logarithmic radial grid
-## Decrease loggrid in intervals of 100
-## At each value, run AtomPAW to generate .UPF and use this to run QE on FCC and BCC structures
-## If differences between original and new values (energy and lattice constants) become too high, stop
+## Optimize the number of points in the logarithmic radial grid.
+## Decrease loggrid in intervals of 100.
+## At each value, run AtomPAW to generate .UPF and use this to run QE on FCC and BCC structures.
+## If differences between original and new values (energy and lattice constants) become too high, stop.
+## Also stop if QE runs into error, e.g., S matrix not positive definite.
+## Tolerances may be changed depending on desired degree of accuracy.
+
+## Usage: python optimize_loggrid.py elem_name
+
+def main():
+
+    elem = sys.argv[-1]
+    ediff = 0
+    etol = 1e-6
+    FCC_diff = 0
+    FCC_tol = 1e-4
+    BCC_diff = 0
+    BCC_tol = 1e-4
+    run_atompaw(elem)
+    benchmark_e = read_output(elem)[1]
+    run_QE(elem,'FCC')
+    benchmark_FCC = get_lattice_constant(elem,'FCC')
+    run_QE(elem,'BCC')
+    benchmark_BCC = get_lattice_constant(elem,'BCC')
+    differences = []
+    check = True
+
+    while (ediff < etol) and (FCC_diff < FCC_tol) and (BCC_diff < BCC_tol) and check == True:
+        filename = elem+'.atompaw.in'
+        with open(filename) as f:
+            lines = f.readlines()
+        log_line = (lines[1]).split()
+        index = 1
+        for word in log_line:
+            if word == 'loggrid':
+                num_pts = float(log_line[index])
+                break
+            else:
+                index += 1
+        num_pts -= 100
+        log_line[index] = str(num_pts)
+        new_line = ''
+        for word in log_line:
+            var = word+' '
+            new_line += var
+        lines[1] = new_line+'\n'
+        with open(filename,'w') as f:
+            for line in lines:
+                f.write(line)
+        try:
+            run_atompaw(elem)
+            ediff = abs(benchmark_e - read_output(elem)[1])/benchmark_e
+            run_QE(elem,'FCC')
+            new_lat_FCC = get_lattice_constant(elem,'FCC')
+            FCC_diff = abs(benchmark_FCC - new_lat_FCC)/benchmark_FCC
+            FCC_walltime = get_walltime(elem,'FCC')
+            run_QE(elem,'BCC')
+            new_lat_BCC = get_lattice_constant(elem,'BCC')
+            BCC_diff = abs(benchmark_BCC - new_lat_BCC)/benchmark_BCC
+            BCC_walltime = get_walltime(elem,'BCC')
+            differences.append([num_pts,ediff,FCC_diff,BCC_diff,FCC_walltime,BCC_walltime])
+        except:
+            check = False
+
+    num_pts += 100
+    log_line[index] = str(num_pts)
+    new_line = ''
+    for word in log_line:
+        var = word+' '
+        new_line += var
+    lines[1] = new_line+'\n'
+    with open(filename,'w') as f:
+        for line in lines:
+            f.write(line)
+
+    print (differences)
+
+
 
 def read_output(elem):
     with open(elem) as f:
@@ -74,66 +148,7 @@ def get_walltime(elem,lat_type):
     time = pwscf_lines[-1][2]
     return time
 
-## Tolerances listed below may need to be tuned
 
-elem = sys.argv[-1]
-ediff = 0
-etol = 1e-6
-FCC_diff = 0
-FCC_tol = 0.0001
-BCC_diff = 0
-BCC_tol = 0.0001
-run_atompaw(elem)
-benchmark_e = read_output(elem)[1]
-run_QE(elem,'FCC')
-benchmark_FCC = get_lattice_constant(elem,'FCC')
-run_QE(elem,'BCC')
-benchmark_BCC = get_lattice_constant(elem,'BCC')
-differences = []
+if __name__=='__main__':
+    main()
 
-while (ediff < etol) and (FCC_diff < FCC_tol) and (BCC_diff < BCC_tol):
-    filename = elem+'.atompaw.in'
-    with open(filename) as f:
-        lines = f.readlines()
-    log_line = (lines[1]).split()
-    index = 1
-    for word in log_line:
-        if word == 'loggrid':
-            num_pts = float(log_line[index])
-            break
-        else:
-            index += 1
-    num_pts -= 100
-    log_line[index] = str(num_pts)
-    new_line = ''
-    for word in log_line:
-        var = word+' '
-        new_line += var
-    lines[1] = new_line+'\n'
-    with open(filename,'w') as f:
-        for line in lines:
-            f.write(line)
-    run_atompaw(elem)
-    ediff = abs(benchmark_e - read_output(elem)[1])/benchmark_e
-    run_QE(elem,'FCC')
-    new_lat_FCC = get_lattice_constant(elem,'FCC')
-    FCC_diff = abs(benchmark_FCC - new_lat_FCC)/benchmark_FCC
-    FCC_walltime = get_walltime(elem,'FCC')
-    run_QE(elem,'BCC')
-    new_lat_BCC = get_lattice_constant(elem,'BCC')
-    BCC_diff = abs(benchmark_BCC - new_lat_BCC)/benchmark_BCC
-    BCC_walltime = get_walltime(elem,'BCC')
-    differences.append([num_pts,ediff,FCC_diff,BCC_diff,FCC_walltime,BCC_walltime])
-
-num_pts += 100
-log_line[index] = str(num_pts)
-new_line = ''
-for word in log_line:
-    var = word+' '
-    new_line += var
-lines[1] = new_line+'\n'
-with open(filename,'w') as f:
-    for line in lines:
-        f.write(line)
-
-print (differences)

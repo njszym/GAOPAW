d02ba1904040ae8e890b428ddc5296ffd7928939
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-07-30 11:53:05 -0400

    Updated get_best_soln.py to include var and obj labels

--- scripts/get_best_soln.py
+++ scripts/get_best_soln.py
@@ -1,51 +1,59 @@
-import math
-import os
-import pandas as pd
-import sys
-import numpy as np
-from shutil import copyfile
-import sys
+from gaopaw import *
 
 
-df = pd.read_table('dakota_tabular.dat',sep='\s+',header=None)
-result_matrix = np.array(df)[1:].transpose()
-num_objs = int(sys.argv[-1])
-slice_index = -1
-obj_fn_matrix = []
-for i in range(num_objs):
-    obj_fn_matrix.append(result_matrix[slice_index])
-    slice_index -= 1
-obj_fn_matrix.reverse()
-new_matrix = []
-for obj_fn_list in obj_fn_matrix:
-    new_list = []
-    for value in obj_fn_list:
-        if round(float(value),1) != 100.0:
-            new_list.append(float(value))
-    new_matrix.append(new_list)
-obj_fn_matrix = np.array(new_matrix)
-min_values = []
-max_values = []
-for obj_fn_list in obj_fn_matrix:
-    min_values.append(min(obj_fn_list))
-    max_values.append(max(obj_fn_list))
-obj_fn_matrix = obj_fn_matrix.transpose()
-rms_errors = []
-for result_set in obj_fn_matrix:
-    result_index = 0
-    norm_errors = []
-    for value in result_set:
-        utopia = min_values[result_index]
-        nadir = max_values[result_index]
-        norm_errors.append( abs( (value - utopia)/(nadir - utopia) ) )
-        result_index += 1
-    rms = 0
-    for value in norm_errors:
-        rms += value
-    rms = rms/len(norm_errors)
-    rms_errors.append(rms)
-min_rms = min(rms_errors)
-min_index = np.argmin(rms_errors)
-final_result = obj_fn_matrix[min_index]
-for value in final_result:
-    print(round(value,5))
+def main():
+    """
+    Get best solution from dakota_tabular.dat
+    """
+    with open('input.json') as input:
+        input_settings = json.load(input, object_hook=lambda d: SimpleNamespace(**d))
+    template_dir = input_settings.directories.elem_template_dir
+    cmpd_list = input_settings.compounds
+    element_list = []
+    for cmpd in cmpd_list:
+        element_list.extend(parse_elems(cmpd.formula))
+    element_list = list(set(element_list))
+    num_objs = get_num_objs(cmpd_list, element_list)
+    cmpd_dict = form_cmpd_dict(cmpd_list)
+    elem_dict = form_element_dict(element_list, template_dir)
+    obj_fn_dict = merge_dicts(elem_dict, cmpd_dict)
+    label_list = dict_to_list(obj_fn_dict)[1]
+    df = pd.read_table('dakota_tabular.dat',sep='\s+',header=None)
+    col_headers = np.array(df)[0][2:]
+    var_headers = col_headers[:-num_objs]
+    obj_headers = col_headers[-num_objs:]
+    value_table = np.array(df)[1:][:, 2:]
+    var_table = value_table[:, :-num_objs]
+    obj_table = value_table[:, -num_objs:]
+    good_vars, good_objs = [], []
+    for (var_list, obj_list) in zip(var_table, obj_table):
+        if '100' not in obj_list:
+            good_vars.append([float(value) for value in var_list])
+            good_objs.append([float(value) for value in obj_list])
+    min_values, max_values = [], []
+    for all_obj_values in np.array(good_objs).transpose():
+        min_values.append(min(all_obj_values))
+        max_values.append(max(all_obj_values))
+    mae_list = []
+    for result_set in good_objs:
+        obj_index = 0
+        normalized_err = []
+        for value in result_set:
+            utopia = min_values[obj_index]
+            nadir = max_values[obj_index]
+            normalized_err.append( abs( (value - utopia)/(nadir - utopia) ) )
+            obj_index += 1
+        mae_list.append(np.average(normalized_err))
+    best_result_set = good_objs[np.argmin(mae_list)]
+    best_var_set = good_vars[np.argmin(mae_list)]
+    for (label, var) in zip(var_headers, best_var_set):
+        print(label, var)
+    for (label, obj) in zip(obj_headers, best_result_set):
+        print(label, obj)
+
+
+
+if __name__=='__main__':
+    main()
+
+

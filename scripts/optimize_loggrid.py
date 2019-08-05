ef216744c696b4c574bcd5c23602b008b70037d1
Nathan Szymanski <szymansk@nyc-desk-l205.schrodinger.com>
2019-08-05 09:29:58 -0400

    Fixed path in scripts, now working

--- scripts/optimize_loggrid.py
+++ scripts/optimize_loggrid.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 from gaopaw import *
 
 ## Usage: python optimize_loggrid.py [elem name]
--- scripts/write_dakota.py
+++ scripts/write_dakota.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.getcwd(), os.pardir))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 from gaopaw import *
 
 
